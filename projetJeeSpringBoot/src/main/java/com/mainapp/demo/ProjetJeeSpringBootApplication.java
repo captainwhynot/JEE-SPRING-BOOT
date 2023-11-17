7f63798c4a403429de6a80c21023ae79f3178853
captainwhynot <83647053+captainwhynot@users.noreply.github.com>
2023-11-17 18:14:37 +0100

    Setup SB

---
+++ projetJeeSpringBoot/src/main/java/com/mainapp/demo/ProjetJeeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.mainapp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetJeeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetJeeSpringBootApplication.class, args);
+	}
+
+}
